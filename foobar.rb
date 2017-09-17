d5cd074eeaebad9348ab507daf951c09323e3437
Kin Seng Chau <kinseng529@gmail.com>
2017-09-17 03:43:51 -0700

    hw1

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,12 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    a.map! { |a| a.to_i + 2 }
+    a.uniq!
+    a.delete_if { |a| a % 2 == 1 || a > 10}
+    b = 0
+    a.each do |a| b = b + a end
+    b
   end
 end
 
